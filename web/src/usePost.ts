715e2d8f3eda9650e56bd34cda04f6e7de1cc71f
MonicaHillman <monicamhillman@gmail.com>
2023-05-10 14:45:29 -0300

    video 3.1

---
+++ web/src/usePost.ts
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+
+export default function usePost() {
+    const [erro, setErro] = useState('');
+    const [sucesso, setSucesso] = useState(false);
+
+    async function cadastrarDados<T>({url, dados} : 
+        {url: string, dados: T}) {
+            try {
+            await fetch(`http://localhost:8080/${url}`, {
+                method: 'POST',
+                body: JSON.stringify(dados)
+            })
+            setSucesso(true);
+        } catch (erro) {
+        setErro('Não foi possível enviar os dados');
+    }
+}
+
+return {cadastrarDados, sucesso, erro}
+}
