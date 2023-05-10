26319b15d11019fcbe271ef41f39e480412fbee5
MonicaHillman <monicamhillman@gmail.com>
2023-05-10 09:29:01 -0300

    termino do desafio

--- web/src/AppRoutes.tsx
+++ web/src/AppRoutes.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import PaginaBase from "./pages/PaginaBase";
+import PaginaInicial from "./pages/Login";
 
 function AppRoutes() {
     return (
         <BrowserRouter>
         <Routes>
             <Route path="/" element={<PaginaBase />}>
+            <Route index element={<PaginaInicial />} />
             <Route path="/dashboard" element={<Dashboard />} />
             </Route>
         </Routes>
