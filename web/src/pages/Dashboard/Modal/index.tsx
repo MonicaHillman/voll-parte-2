7ea6b030a4092d8857d23d48da11bc358396bb8a
monicahillman <mmhillman1@ucs.br>
2023-05-10 23:29:56 -0300

    desafio 1 aula 05

--- web/src/pages/Dashboard/Modal/index.tsx
+++ web/src/pages/Dashboard/Modal/index.tsx
@@ -1,6 +1,11 @@
-import { Box, Modal } from "@mui/material";
-import styled from "styled-components";
+import { useState } from "react";
+import { Checkbox, FormControlLabel, FormGroup, Modal, Switch } from '@mui/material';
+import { Box } from "@mui/material";
 import Titulo from "../../../components/Titulo";
+import styled from "styled-components";
+import CampoDigitacao from "../../../components/CampoDigitacao";
+import Botao from "../../../components/Botao";
+import Subtitulo from "../../../components/Subtitulo";
 
 const BoxCustomizado = styled(Box)`
   position: fixed;
@@ -16,7 +21,57 @@ const BoxCustomizado = styled(Box)`
   padding: 1em 5em;
 `;
 
+const Container = styled.div`
+text-align: left;
+`
+
+const ContainerSwitch = styled.div`
+text-align: center;
+`
+
+const TextoSwitch = styled.p`
+color: var(--cinza);
+`
+
+const BotaoCustomizado = styled(Botao)`
+    width: 50%;
+    display: block;
+    margin: 0 auto;
+`
+
+const ContainerEndereco = styled.div`
+display: grid;
+grid-template-columns: 2fr 1fr;
+grid-gap: 0 1em;
+`
+
 export default function ModalCadastro({ open, handleClose }: { open: boolean, handleClose: () => void }) {
+    const [planosSelecionados, setPlanosSelecionados] = useState<string[]>([]);
+    const [nome, setNome] = useState("");
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+    const [senhaVerificada, setSenhaVerificada] = useState("");
+    const [possuiPlano, setPossuiPlano] = useState(false);
+    const [imagem, setImagem] = useState('');
+    const [especialidade, setEspecialidade] = useState("");
+    const [crm, setCrm] = useState("");
+    const [cep, setCep] = useState("");
+    const [rua, setRua] = useState("");
+    const [numero, setNumero] = useState("");
+    const [complemento, setComplemento] = useState("");
+    const [estado, setEstado] = useState("");
+    const [telefone, setTelefone] = useState("");
+    const label = { inputProps: { 'aria-label': 'Atende por plano?' } };
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const checkboxValue = event.target.value;
+        if (event.target.checked) {
+            setPlanosSelecionados([...planosSelecionados, checkboxValue]);
+        } else {
+            setPlanosSelecionados(planosSelecionados.filter(plano => plano !== checkboxValue));
+        }
+    };
+
     return (
         <>
             <Modal
@@ -27,6 +82,78 @@ export default function ModalCadastro({ open, handleClose }: { open: boolean, ha
             >
                 <BoxCustomizado>
                     <Titulo>Cadastre o especialista inserindo os dados abaixo:</Titulo>
+                    <form>
+                        <Container>
+                            <CampoDigitacao tipo="text" label="Nome" valor={nome} placeholder="Digite seu nome completo" onChange={setNome} />
+                            <CampoDigitacao tipo="email" label="Email" valor={email} placeholder="Digite seu email" onChange={setEmail} />
+                            <CampoDigitacao tipo="password" label="Senha" valor={senha} placeholder="Digite sua senha" onChange={setSenha} />
+                            <CampoDigitacao tipo="password" label="Repita a senha" valor={senhaVerificada} placeholder="Digite sua senha novamente" onChange={setSenhaVerificada} />
+                            <CampoDigitacao tipo="text" label="Especialidade" valor={especialidade} placeholder="Qual sua especialidade?" onChange={setEspecialidade} />
+                            <CampoDigitacao tipo="number" label="CRM" valor={crm} placeholder="Insira seu número de registro" onChange={setCrm} />
+                            <CampoDigitacao tipo="tel" label="Telefone" valor={telefone} placeholder="(DDD) XXXXX-XXXX" onChange={setTelefone} />
+                            <CampoDigitacao tipo="text" label="Insira a URL da sua imagem" valor={imagem} placeholder="https://img.com/fotos/retrato-de-um-jovem-medico" onChange={setImagem} />
+
+                            <CampoDigitacao
+                                tipo="number"
+                                label="Endereço"
+                                valor={cep}
+                                placeholder="Insira o CEP"
+                                onChange={setCep}
+                            />
+                            <ContainerEndereco>
+                                <CampoDigitacao
+                                    tipo="text"
+                                    valor={rua}
+                                    placeholder="Rua"
+                                    onChange={setRua}
+                                />
+                                <CampoDigitacao
+                                    tipo="number"
+                                    valor={numero}
+                                    placeholder="Número"
+                                    onChange={setNumero}
+                                />
+                                <CampoDigitacao
+                                    tipo="text"
+                                    valor={complemento}
+                                    placeholder="Complemento"
+                                    onChange={setComplemento}
+                                />
+                                <CampoDigitacao
+                                    tipo="text"
+                                    valor={estado}
+                                    placeholder="Estado"
+                                    onChange={setEstado}
+                                />
+                            </ContainerEndereco>
+                        </Container>
+
+                        <ContainerSwitch>
+                            <Subtitulo>Atende por plano?</Subtitulo>
+                            <Switch {...label} onChange={() => {
+                                possuiPlano ?
+                                    setPossuiPlano(false) :
+                                    setPossuiPlano(true)
+                            }
+                            } />
+                            <TextoSwitch>Não/Sim</TextoSwitch>
+                        </ContainerSwitch>
+                        {possuiPlano ?
+                            <>
+                                <Subtitulo>Selecione os planos:</Subtitulo>
+                                <FormGroup>
+                                    <FormControlLabel control={<Checkbox onChange={handleChange} value="Sulamerica" />} label="Sulamerica" />
+                                    <FormControlLabel control={<Checkbox onChange={handleChange} value="Unimed" />} label="Unimed" />
+                                    <FormControlLabel control={<Checkbox onChange={handleChange} value="Bradesco" />} label="Bradesco" />
+                                    <FormControlLabel control={<Checkbox onChange={handleChange} value="Amil" />} label="Amil" />
+                                    <FormControlLabel control={<Checkbox onChange={handleChange} value="Biosaúde" />} label="Biosaúde" />
+                                    <FormControlLabel control={<Checkbox onChange={handleChange} value="Biovida" />} label="Biovida" />
+                                    <FormControlLabel control={<Checkbox onChange={handleChange} value="Outro" />} label="Outro" />
+                                </FormGroup>
+                            </>
+                            : ''}
+                        <BotaoCustomizado>Cadastrar</BotaoCustomizado>
+                    </form>
                 </BoxCustomizado>
             </Modal >
         </>
